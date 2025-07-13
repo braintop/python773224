c3560972571c61c5d0203d1befd18bbf73f0f3f4
ewfewfwef <braintop7@gmail.com>
2025-07-13 20:54:11 +0300

    add

--- 13.7-react-map-trinary/04-react-list/src/App.jsx
+++ 13.7-react-map-trinary/04-react-list/src/App.jsx
@@ -1,5 +1,6 @@
 import { ProductList } from "./Components/ProductList"
 import { StudentList } from "./Components/StudentList"
+import { ProductListV2 } from "./Components/ProductListV2"
 
 function App() {
 
@@ -8,6 +9,9 @@ function App() {
       <h1>map</h1>
       <StudentList />
       <ProductList />
+      <p>--------------------</p>
+      <ProductListV2 />
+
     </>
   )
 }
