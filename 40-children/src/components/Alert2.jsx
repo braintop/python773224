3a566ae3a3898cc5956d79d5a1fdb0f0bbefe01a
braintop <braintop7@gmail.com>
2025-11-09 18:56:28 +0200

    Add

---
+++ 40-children/src/components/Alert2.jsx
@@ -0,0 +1,9 @@
+import styles from './Alert2.module.css'
+
+export default function Alert2({type="success",children}) {
+    return (
+        <div className={`${styles.alert} ${styles[type]}`}>
+            {children}
+        </div>
+    )
+}
